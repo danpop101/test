97371e8e927fc04facb2f9a78e83275d559373c1
Daniel Pop <dpop100@gmail.com>
2024-01-08 16:08:02 +0200

    add QrReader

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,27 @@
+"use client";
+import { useState } from "react";
+import { QrReader } from "react-qr-reader";
+
 export default function Home() {
-  return <main>Test</main>;
+  const [data, setData] = useState("No result");
+
+  return (
+    <>
+      <QrReader
+        onResult={(result, error) => {
+          if (!!result) {
+            setData(result?.getText);
+          }
+
+          if (!!error) {
+            console.info(error);
+          }
+        }}
+        containerStyle={{ height: 240, width: 320 }}
+        scanDelay={500}
+        constraints={{ facingMode: "user" }}
+      />
+      <p>{data}</p>
+    </>
+  );
 }
