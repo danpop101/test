6c1e9a83f37476f14e9f93b5c12ea66a9fc4b61f
Daniel Pop <dpop100@gmail.com>
2024-01-08 21:12:23 +0000

    Update 4 files

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,9 @@
-"use client";
-import { useState } from "react";
-import { QrReader } from "react-qr-reader";
+import { ScanQr } from "@/components/scan-qr";
 
 export default function Home() {
-  const [data, setData] = useState("No result");
-  var audio = new Audio("./beep.wav");
-
   return (
-    <div className="flex flex-col items-center justify-center gap-8">
-      <div className="flex-1 mt-4 min-h-[430px]">
-        <QrReader
-          onResult={(result, error) => {
-            if (!!result) {
-              setData(result?.getText().toString());
-              audio.play();
-            }
-
-            if (!!error) {
-              console.info(error);
-            }
-          }}
-          containerStyle={{ height: 240, width: 450 }}
-          scanDelay={2000}
-          constraints={{ facingMode: "environment" }}
-        />
-      </div>
-      <div className="flex-1">
-        <p>{data}</p>
-      </div>
-    </div>
+    <>
+      <ScanQr />
+    </>
   );
 }
